48f7342f0fd99316a63fa388b86a66eba5de3166
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-03-29 13:37:21 +0200

    import, add StoreProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import StoreProvider from './store/StoreProvider';
+
 import './App.scss';
 
 const App = () => {
   return ( 
-    <>
+    <StoreProvider>
     <header>Hello world</header>
-    </>
+    </StoreProvider>
    );
 }
  
