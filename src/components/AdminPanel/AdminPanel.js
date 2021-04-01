4107d24ece0ec75ff85fad805ad560d578c76611
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-04-01 14:29:55 +0200

    feat: create Admin

---
+++ src/components/AdminPanel/AdminPanel.js
@@ -0,0 +1,36 @@
+import React, { useContext, useState } from "react";
+
+import CourseDetails from "./subcomponents/CoursesDetails";
+import CoursePopup from "./subcomponents/CoursePopup";
+import { StoreContext } from "../../store/StoreProvider";
+
+const AdminPanel = () => {
+  const [isOpenPopup, setIsOpenPopup] = useState(false);
+  const { courses } = useContext(StoreContext);
+
+  const showPopup = () => setIsOpenPopup(true);
+  const hidePopup = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    setIsOpenPopup(false);
+  };
+
+  const coursesElements = courses.map((course) => (
+    <CourseDetails key={course.id} {...course} />
+  ));
+
+  return (
+    <section>
+      {coursesElements}
+      <button onClick={showPopup}>Dodaj nowy kurs</button>
+      <CoursePopup
+        isEditMode={false}
+        isOpenPopup={isOpenPopup}
+        hidePopup={hidePopup}
+      />
+    </section>
+  );
+};
+
+export default AdminPanel;
